081a57b464ea205835b1c9a6dec0b6530883726c
Teddy GRAVELEAU <teddy.graveleau@agena3000.com>
2021-12-11 13:51:19 +0100

    Puzzle 11, les flash : très cool

---
+++ 11/puzzle_1.php
@@ -0,0 +1,83 @@
+<?php
+$debut = microtime(true);
+$string = include("data.php");
+// $string = include("data_test.php");
+$dataBrut = preg_split('/[\s]+/', $string);
+$octopuses = [];
+foreach ($dataBrut as $db) {
+    $octopuses[] = array_map('intval', str_split($db));
+}
+
+$DAYS = 100;
+$NB_LIGNES = count($octopuses);
+$NB_COLONNES = count($octopuses[0]);
+
+$nbFlashes = 0;
+for ($day=0; $day<$DAYS; $day++) {
+    // Energy +1
+    for ($i=0; $i<$NB_LIGNES; $i++) {
+        for ($j=0; $j<$NB_COLONNES; $j++) {
+            $octopuses[$i][$j]++;
+        }
+    }
+    // Flash
+    for ($i=0; $i<$NB_LIGNES; $i++) {
+        for ($j=0; $j<$NB_COLONNES; $j++) {
+            $nbFlashes += flash($octopuses, $i, $j, $NB_LIGNES, $NB_COLONNES);
+        }
+    }
+}
+
+echo "Flashes : $nbFlashes\n";
+
+function flash(&$octopuses, $i, $j, $nbLignes, $nbColonnes, $neighborHasFlashed = false) : int {
+    // S'il n'a pas déjà flashé et qu'on vient de flashé à côté de lui, il gagne de l'énérgie
+    if ($octopuses[$i][$j] != 0 && $neighborHasFlashed) {
+        $octopuses[$i][$j]++;
+    }
+    // Pas de flash
+    if ($octopuses[$i][$j] < 10) {
+        return 0;
+    }
+    // On met à 0 pour le jour d'après et pour ne pas reflasher
+    $octopuses[$i][$j] = 0;
+    // On ajoute 1 et on flash les voisins
+    $nbFlash = 1;
+    // à gauche
+    if ($i != 0) {
+        $nbFlash += flash($octopuses, $i-1, $j, $nbLignes, $nbColonnes, true);
+        // en haut à gauche
+        if ($j != 0) {
+            $nbFlash += flash($octopuses, $i-1, $j-1, $nbLignes, $nbColonnes, true);
+        }
+    }
+    // à droite
+    if ($i != ($nbLignes-1)) {
+        $nbFlash += flash($octopuses, $i+1, $j, $nbLignes, $nbColonnes, true);
+        // en bas à droite
+        if ($j != ($nbColonnes-1)) {
+            $nbFlash += flash($octopuses, $i+1, $j+1, $nbLignes, $nbColonnes, true);
+        }
+    }
+    // en haut
+    if ($j != 0) {
+        $nbFlash += flash($octopuses, $i, $j-1, $nbLignes, $nbColonnes, true);
+        // en haut à droite
+        if ($i != ($nbLignes-1)) {
+            $nbFlash += flash($octopuses, $i+1, $j-1, $nbLignes, $nbColonnes, true);
+        }
+    }
+    // en bas
+    if ($j != ($nbColonnes-1)) {
+        $nbFlash += flash($octopuses, $i, $j+1, $nbLignes, $nbColonnes, true);
+        // en bas à gauche
+        if ($i != 0) {
+            $nbFlash += flash($octopuses, $i-1, $j+1, $nbLignes, $nbColonnes, true);
+        }
+    }
+    return $nbFlash;
+}
+
+$end = microtime(true);
+echo "Durée : ".($end-$debut)." sec\n";
+?>
